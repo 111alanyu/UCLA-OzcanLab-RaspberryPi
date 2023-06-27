fee666c5499f123885936dbec1b585856ed28db0
111alanyu <alanyu111@ucla.edu>
2023-06-26 21:13:53 -0700

    Image added to Analysis

--- analysis_ui.py
+++ analysis_ui.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
 from quantify_VFA_timelapse_decap import *
+from PIL import ImageTk, Image
+import os
 
 
 class MyFrame(tk.Frame):
@@ -29,8 +31,24 @@ class MyFrame(tk.Frame):
         self.begin_anal_button = tk.Button(self, text = "Begin Analysis", command = self.ui_holder)
         self.begin_anal_button.pack()
         
+        
+        self.image = Image.open("/home/pi/Desktop/campus-seal.jpg")
+        self.resized_image = self.image.resize((250,200))
+        self.image_tk = ImageTk.PhotoImage(self.resized_image)
+        self.image_label = tk.Label(self, image=self.image_tk)
+        self.image_label.pack()
+        
+        
     def ui_holder(self):
         main_ui_analysis(self.path_entry.get())
+        self.process_image = self.path_entry.get() + "/processed_jpgs"
+        image_address = [os.path.join(self.process_image, file) for file in os.listdir(self.process_image)]
+        
+        self.image = Image.open(image_address[0])
+        self.resized_image = self.image.resize((250,200))
+        self.image_tk = ImageTk.PhotoImage(self.resized_image)
+        self.image_label = tk.Label(self, image=self.image_tk)
+        self.image_label.pack()
         
     def pool_input(self):
         self.path_val = self.path_entry.get()
